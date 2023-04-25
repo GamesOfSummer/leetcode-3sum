abe6e49c650bb23dc07129df2ee25891df2129be
Summer T <summer@summertime-development.com>
2023-04-25 13:50:22 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -5,73 +5,50 @@ import {
     validateFxn,
 } from './helpers.js';
 
-function threeSum(array: Array<number>): any {
-    if (array.length < 3) {
+function threeSum(array: any): any {
+    if (array.length < 1) {
         return [];
     }
 
-    array = array.sort((a, b) => a - b);
+    array = array.sort((a, b) => a[0] - b[0]);
 
-    let triplets = [];
-    let left = 0;
-    let right = 0;
+    let finalArray: number[][] = [];
 
-    for (let target = 0; target < array.length; target++) {
-        while (
-            target !== 0 &&
-            target + 1 < array.length &&
-            array[target] === array[target - 1]
-        ) {
-            target++;
+    let queue = [];
+
+    queue.push[array[0]];
+    for (let i = 1; i < array.length; i++) {
+        let [x, y] = queue.pop();
+        let [x2, y2] = array[i];
+
+        if (y > x2) {
+            finalArray.push([x, y2]);
         }
 
-        left = target + 1;
-        right = array.length - 1;
-
-        while (left < right) {
-            const sum = array[target] + array[left] + array[right];
-            if (sum === 0) {
-                triplets.push([array[target], array[left], array[right]]);
-
-                left++;
-                while (array[left] === array[left - 1] && left < right) {
-                    // prevent dups
-                    left++;
-                }
-            } else if (sum > 0) {
-                right--;
-            } else {
-                left++;
-            }
-        }
+        queue.push(array[i]);
     }
 
-    return triplets;
+    return finalArray;
 }
 
 consoleStart();
 
-// **-1,-1,-2,-3,-4,0,0,1,2,3,4,
-
-validateFxn(threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]), [
-    [
-        [-4, 0, 4],
-        [-4, 1, 3],
-        [-3, -1, 4],
-        [-3, 0, 3],
-        [-3, 1, 2],
-        [-2, -1, 3],
-        [-2, 0, 2],
-        [-1, -1, 2],
-        [-1, 0, 1],
-    ],
-]);
-
 // -1, -1, 0, 1, 2, -4
-validateFxn(threeSum([-1, 0, 1, 2, -1, -4]), [
-    [-1, -1, 2],
-    [-1, 0, 1],
-]);
+validateFxn(
+    threeSum([
+        [1, 3],
+        [2, 6],
+        [8, 10],
+        [15, 18],
+    ]),
+    [
+        [
+            [1, 6],
+            [8, 10],
+            [15, 18],
+        ],
+    ]
+);
 
 consoleEnd();
 
